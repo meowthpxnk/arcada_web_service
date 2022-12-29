d942014705685738cd58e193987230f93316b2fa
MeowthPxnk <meowthpxnk@gmail.com>
2022-12-29 15:13:09 +1000

    commited

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 module.exports = {
-  api_url: "https://api.arcadakms.ru/",
+  api_url: "http://localhost:5000/",
 }
--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
 	transpileDependencies: true,
 
 	devServer: {
-		host: '127.0.0.1',
+		host: 'localhost',
 		port: 8080,
 	},
 })
