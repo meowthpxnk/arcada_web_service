b693d53add648c3ea26c6fba77bf38966218ebd8
MeowthPxnk <meowthpxnk@gmail.com>
2023-01-17 23:49:43 +1000

    commited

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,5 @@
 module.exports = {
   // api_url: "http://127.0.0.1:5050/",
-  api_url: "https://api.arcadakms.ru/"
+  // api_url: "https://api.arcadakms.ru/",
+  api_url: "https://admincafe.ru/",
 }
