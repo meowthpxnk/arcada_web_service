4242c8608ba3891cafe22a6493c8d1da347af9b7
MeowthPxnk <119928123+meowthpxnk@users.noreply.github.com>
2023-03-17 10:44:13 +0300

    commited

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  api_url: "http://127.0.0.1:5000/",
+  // api_url: "http://127.0.0.1:5000/",
   // api_url: "https://api.arcadakms.ru/",
-  // api_url: "https://admincafe.ru/",
+  api_url: "https://admincafe.ru/",
   // api_url: "http://localhost:5050/",
 }
